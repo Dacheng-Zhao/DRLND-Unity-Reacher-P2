045cce0f371dfa6522cbfc44aaa2bb0a7c4b65ab
Dacheng Zhao <daczhao123@gmail.com>
2019-01-02 10:10:48 -0500

    Add comment to agent norm gradient

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -101,6 +101,21 @@ class Agent():
         # Minimize the loss
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
+        """Clips gradient norm of an iterable of parameters.
+
+        The norm is computed over all gradients together, as if they were
+        concatenated into a single vector. Gradients are modified in-place.
+
+        Arguments:
+        parameters (Iterable[Tensor] or Tensor): an iterable of Tensors or a
+            single Tensor that will have gradients normalized
+        max_norm (float or int): max norm of the gradients
+        norm_type (float or int): type of the used p-norm. Can be ``'inf'`` for
+            infinity norm.
+
+        Returns:
+            Total norm of the parameters (viewed as a single vector).
+        """
         torch.nn.utils.clip_grad_norm_(self.critic_local.parameters(), 1)
         self.critic_optimizer.step()
 
